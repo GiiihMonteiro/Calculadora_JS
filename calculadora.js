65b060f57d8bd0fa0ae7c7086273720d7cb2fb1f
GiiihMonteiro <giovannacratz06@gmail.com>
2025-11-06 10:34:03 -0300

    exercicios JS

--- calculadora.js
+++ calculadora.js
@@ -1,10 +1,13 @@
 //
 
 function calcular(){
-    let numero1 = document.getElementById("numero1")
-    let numero2 = document.getElementById("numero2")
+    let numero1 = Number (document.getElementById("numero1").value)
+    let numero2 = Number (document.getElementById("numero2").value)
 
     let mensagem = document.getElementById("mensagem")
 
+    let soma = numero1 + numero2;
+
+    mensagem.innerText = `O resultado é ${soma}`
 
 }
